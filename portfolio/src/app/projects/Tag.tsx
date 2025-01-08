656eaadcee5357aa7c8f9f45106dcc0d563cb541
SowonOh <sowon5oh@gmail.com>
2025-01-09 02:28:45 +0900

    fix

--- portfolio/src/app/projects/Tag.tsx
+++ portfolio/src/app/projects/Tag.tsx
@@ -10,7 +10,7 @@ export default function Tag({ tag }: TagProps) {
     <span 
       className="inline-flex items-center px-2 py-1 text-[10px] pixel-corners border border-white"
     >
-      <TagIcon size={12} className="mr-1" />
+      <TagIcon size={12} className="mr-1" iconNode={[]} />
       {tag}
     </span>
   );
